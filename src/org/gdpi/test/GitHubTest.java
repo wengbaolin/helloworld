bdae42d0565cf97551a11aac33e3bb074f6230c7
baron <81132217@qq.com>
2019-03-26 14:39:58 +0800

    v0.1

--- src/org/gdpi/test/GitHubTest.java
+++ src/org/gdpi/test/GitHubTest.java
@@ -2,6 +2,6 @@ package org.gdpi.test;
 
 public class GitHubTest {
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub!3.26");
 	}
 }
